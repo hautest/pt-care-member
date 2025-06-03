8cf5dac4c573a02ff82810d1e1de0b083efc98fd
hautest <dos07008@naver.com>
2025-06-03 15:07:21 +0900

    feat: my 페이지 기능 추가 및 react-native-mmkv 추가

--- src/screens/(tabs)/my.tsx
+++ src/screens/(tabs)/my.tsx
@@ -1,15 +1,103 @@
+import { useGetUserSuspenseQuery } from "@features/user/useGetUserSuspenseQuery";
+import { useLogoutMutation } from "@features/user/useLogoutMutation";
+import { queryClient } from "@shared/queryClient/queryClient";
+import { useRouter } from "expo-router";
 import { createStyle, useThemeStyle } from "pt-care-libs";
-import { View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+
+function httpToHttps(url: string) {
+  return url.replace("http://", "https://");
+}
 
 export default function MyScreen() {
   const styles = useThemeStyle(themedStyles);
 
-  return <View style={styles.block}></View>;
+  const router = useRouter();
+
+  const { data } = useGetUserSuspenseQuery();
+  const { name, picture, email } = data.user?.user_metadata || {};
+
+  const { mutate: logout } = useLogoutMutation({
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: useGetUserSuspenseQuery.queryKey,
+      });
+      router.navigate("/");
+      router.navigate("/login");
+    },
+  });
+
+  return (
+    <View style={styles.block}>
+      <View style={styles.header}>
+        <Image
+          source={{ uri: httpToHttps(picture || "") }}
+          style={styles.image}
+        />
+        <View>
+          <View style={styles.titleBlock}>
+            <Text style={styles.title}>안녕하세요 </Text>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.title}>님</Text>
+          </View>
+          <Text style={styles.email}>{email}</Text>
+        </View>
+      </View>
+      <TouchableOpacity
+        onPress={() => logout()}
+        activeOpacity={0.3}
+        style={styles.logoutButton}
+      >
+        <Text style={styles.logoutButtonText}>로그아웃</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-const themedStyles = createStyle(({ themeColor }) => ({
+const themedStyles = createStyle(({ themeColor, typo }) => ({
   block: {
     backgroundColor: themeColor.background.primary,
     flex: 1,
+    padding: 16,
+    justifyContent: "space-between",
+  },
+  header: {
+    alignItems: "center",
+    gap: 16,
+    flexDirection: "row",
+  },
+  title: {
+    fontSize: typo.sizes.h2,
+    fontWeight: typo.weights.medium,
+    color: themeColor.text.primary,
+  },
+  name: {
+    fontSize: typo.sizes.h2,
+    fontWeight: typo.weights.bold,
+    color: themeColor.brand.primary,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+  },
+  titleBlock: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  email: {
+    fontSize: typo.sizes.bodyMedium,
+    fontWeight: typo.weights.regular,
+    color: themeColor.text.tertiary,
+  },
+  logoutButton: {
+    padding: 4,
+    borderRadius: 4,
+    alignSelf: "center",
+  },
+  logoutButtonText: {
+    fontSize: typo.sizes.bodySmall,
+    color: themeColor.text.tertiary,
+    fontWeight: typo.weights.semiBold,
   },
 }));

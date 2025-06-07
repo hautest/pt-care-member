3fabc602bb075a4d107f76ad0efd635016dd8d7f
hautest <dos07008@naver.com>
2025-06-07 16:27:09 +0900

    feat: member manager 전환 기능 추가

--- src/screens/(tabs)/my.tsx
+++ src/screens/(tabs)/my.tsx
@@ -1,7 +1,9 @@
+import { useChangeUserModeMutation } from "@features/user/useChangeUserModeMutation";
 import { useGetUserSuspenseQuery } from "@features/user/useGetUserSuspenseQuery";
 import { useLogoutMutation } from "@features/user/useLogoutMutation";
 import { queryClient } from "@shared/queryClient/queryClient";
 import { createStyle, useThemeStyle } from "@shared/ui/createStyle";
+import { userModeMMKV } from "@shared/utils/userModeMMKV";
 import { useRouter } from "expo-router";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 
@@ -17,6 +19,18 @@ export default function MyScreen() {
   const { data } = useGetUserSuspenseQuery();
   const { name, picture, email } = data.user?.user_metadata || {};
 
+  const [userMode] = userModeMMKV.useMMKV();
+
+  const isManager = userMode === "manager";
+
+  const { mutate: changeUserMode } = useChangeUserModeMutation({
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: useGetUserSuspenseQuery.queryKey,
+      });
+    },
+  });
+
   const { mutate: logout } = useLogoutMutation({
     onSuccess: () => {
       queryClient.invalidateQueries({
@@ -29,27 +43,49 @@ export default function MyScreen() {
 
   return (
     <View style={styles.block}>
-      <View style={styles.header}>
-        <Image
-          source={{ uri: httpToHttps(picture || "") }}
-          style={styles.image}
-        />
-        <View>
-          <View style={styles.titleBlock}>
-            <Text style={styles.title}>안녕하세요 </Text>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.title}>님</Text>
+      <View>
+        <View style={styles.header}>
+          <Image
+            source={{ uri: httpToHttps(picture || "") }}
+            style={styles.image}
+          />
+          <View>
+            <View style={styles.titleBlock}>
+              <Text style={styles.title}>안녕하세요 </Text>
+              <Text style={styles.name}>{name}</Text>
+              <Text style={styles.title}>님</Text>
+            </View>
+            <Text style={styles.email}>{email}</Text>
           </View>
-          <Text style={styles.email}>{email}</Text>
         </View>
+        <Text style={styles.userMode}>
+          현재{" "}
+          <Text style={styles.userModeStrong}>
+            {isManager ? "트레이너" : "회원"}
+          </Text>
+          으로 로그인중이에요.
+        </Text>
       </View>
-      <TouchableOpacity
-        onPress={() => logout()}
-        activeOpacity={0.3}
-        style={styles.logoutButton}
-      >
-        <Text style={styles.logoutButtonText}>로그아웃</Text>
-      </TouchableOpacity>
+      <View style={styles.bottomButtonContainer}>
+        <TouchableOpacity
+          onPress={() =>
+            changeUserMode({ userMode: isManager ? "member" : "manager" })
+          }
+          activeOpacity={0.3}
+          style={styles.bottomButton}
+        >
+          <Text style={styles.bottomButtonText}>
+            {isManager ? "회원으로" : "트레이너로"} 전환하기
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => logout()}
+          activeOpacity={0.3}
+          style={styles.bottomButton}
+        >
+          <Text style={styles.bottomButtonText}>로그아웃</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -65,6 +101,7 @@ const themedStyles = createStyle(({ themeColor, typo }) => ({
     alignItems: "center",
     gap: 16,
     flexDirection: "row",
+    marginBottom: 16,
   },
   title: {
     fontSize: typo.sizes.h2,
@@ -90,12 +127,30 @@ const themedStyles = createStyle(({ themeColor, typo }) => ({
     fontWeight: typo.weights.regular,
     color: themeColor.text.tertiary,
   },
-  logoutButton: {
+  userMode: {
+    fontSize: typo.sizes.bodyMedium,
+    fontWeight: typo.weights.regular,
+    color: themeColor.text.tertiary,
+    borderWidth: 1,
+    borderColor: themeColor.border.light,
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 16,
+    textAlign: "center",
+  },
+  userModeStrong: {
+    fontWeight: typo.weights.bold,
+    color: themeColor.text.primary,
+  },
+  bottomButtonContainer: {
+    gap: 8,
+  },
+  bottomButton: {
     padding: 4,
     borderRadius: 4,
     alignSelf: "center",
   },
-  logoutButtonText: {
+  bottomButtonText: {
     fontSize: typo.sizes.bodySmall,
     color: themeColor.text.tertiary,
     fontWeight: typo.weights.semiBold,
